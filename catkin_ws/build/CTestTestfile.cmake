9675258a2bfb19d7875a4527a7b25fdeb4082b6c
Luke Byrne <lbyrne201@caledonian.ac.uk>
2021-01-27 11:15:40 +0000

    updated jet urdf to include slight imu error, and to publish imu data on /imu. camera fps was decreased to 20fps and camera size changed to 800 x 400.

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/luke/catkin_ws/src
+# Build directory: /home/luke/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("jet_description")
+subdirs("jet_gazebo")
+subdirs("jet_control")
